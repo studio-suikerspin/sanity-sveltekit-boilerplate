37175595b97ba812fa64726f204553ecaaf35708
Robin de Laater <robin@suikerspin.studio>
2026-02-02 13:21:50 +0100

    chore: trigger deploy

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -2,13 +2,17 @@ import { handlePreviewMode, handleQueryLoader, setServerClient } from '@sanity/s
 import { redirect } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { serverClient } from '$lib/cms/client.server';
+import { SANITY_PREVIEW_SECRET } from '$env/static/private';
 
 setServerClient(serverClient);
 
 export const handle = sequence(
 	handlePreviewMode({
 		client: serverClient,
-		preview: { redirect }
+		preview: {
+			redirect,
+			secret: SANITY_PREVIEW_SECRET
+		}
 	}),
 	handleQueryLoader()
 );
